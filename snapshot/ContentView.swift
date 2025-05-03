0984777d6c0b2ef1fe72a27c3755637141333008
cher1shRXD <taewoo080401@gmail.com>
2025-05-03 11:47:11 +0900

    Initial Commit

---
+++ snapshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snapshot
+//
+//  Created by cher1shRXD on 5/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snapshot/snapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  snapshotApp.swift
+//  snapshot
+//
+//  Created by cher1shRXD on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct snapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
